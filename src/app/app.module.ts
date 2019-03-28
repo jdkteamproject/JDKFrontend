4f996fab717ea125d7ab0060533ce117bcd09866
Kelby Behounek <knbehounek@hotmail.com>
2019-03-28 13:08:06 -0400

    angularSETUP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PersonalProfileComponent } from './components/personal-profile/personal-profile.component';
+import { EventsComponent } from './components/events/events.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    PersonalProfileComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
