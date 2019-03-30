3295c834f6946d02bfe56faecd9e447fc15dacaf
Kelby Behounek <knbehounek@hotmail.com>
2019-03-30 18:16:29 -0400

    event/users

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,16 +1,37 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from  '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { EventModel } from "src/app/model/events/events.model";
+import { Friend } from "src/app/model/users/users.model";
+import { from } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataService {
+  baseUrl: string = "http://18.223.119.112:8085/cue";
 
-  baseUrl:string = "'http://18.223.119.112:8085/cue"
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  get_AllEvents(): Promise<EventModel> {
+    return this.http.get<EventModel>(`${this.baseUrl}/api`).toPromise();
+  }
 
-  get_AllEvents(){
-    return this.http.get(this.baseUrl+'/api');
+  getAllUsers() {
+    return this.http.get<Friend[]>(`${this.baseUrl}/users/all`);
+  }
+  getUserById(id: number) {
+    return this.http.get(`${this.baseUrl}/user/` + id);
+  }
+
+  register(user: Friend) {
+    return this.http.post(`${this.baseUrl}/user/`, user);
+  }
+
+  update(user: Friend) {
+    return this.http.put(`${this.baseUrl}/user/` + user.id, user);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.baseUrl}/user/` + id);
   }
 }
