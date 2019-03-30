3e1f4d6e0aeaa5baa1cf64b54acf70e723b4dcc4
VanalstineDJ <VanalstineDJ@gmail.com>
2019-03-30 18:28:11 -0400

    added all necesary components, working on login

--- src/app/components/personal-profile/personal-profile.component.ts
+++ src/app/components/personal-profile/personal-profile.component.ts
@@ -12,4 +12,18 @@ export class PersonalProfileComponent implements OnInit {
   ngOnInit() {
   }
 
+  // declare var modal: string = document.getElementById('profileModal');
+  // var btn = document.getElementById("seeProfile");
+  // var span = document.getElementsByClassName("close")[0];
+  // btn.onclick = function() {
+  //   modal.style.display = "block";
+  // }
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  // }
+  // window.onclick = function(event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // }
 }
