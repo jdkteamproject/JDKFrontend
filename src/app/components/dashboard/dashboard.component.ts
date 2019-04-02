d49b6989aea74f7ecede93e64787db046cd24515
Kelby Behounek <knbehounek@hotmail.com>
2019-04-01 21:52:51 -0400

    searchworks

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.getEvents();
+  }
+
+  events: Object[] = [];
+
+  getEvents(){
+    this.dataService.get_AllUsersEvents().then((res)=>{
+      const data = res;
+      console.log(data._embedded);
+      this.events = data._embedded.events;
+
+    })
+
+    .catch((e)=>console.log(e));
   }
 
 }
